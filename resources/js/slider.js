c5a72053ebed9e88738dd9af30738402a1b92cf6
Milos Sundic <42153865+milossundic@users.noreply.github.com>
2019-10-09 08:46:09 +0200

    Initial Commit

---
+++ resources/js/slider.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('.responsive').slick({
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
